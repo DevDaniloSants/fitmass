02fcfa13c1f9fc043b50479d1f562848fadcadff
Danilo Santos <dscofield@outlook.com.br>
2024-01-10 14:08:15 -0300

    Algumas modificações na config do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+//styles
+import './styles/app.sass';
+
+// components
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="App">
+      <Header></Header>
+      <div className="container"></div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default App;
