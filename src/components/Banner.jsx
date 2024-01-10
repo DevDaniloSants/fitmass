a676fc1c47348a7ab7e89cd2fae64b7aa78d80a6
Danilo Santos <dscofield@outlook.com.br>
2024-01-10 15:55:24 -0300

    Adiciona o banner ao projeto

---
+++ src/components/Banner.jsx
@@ -0,0 +1,26 @@
+// styles
+import '../styles/components/banner.sass';
+
+const Banner = () => {
+  return (
+    <div className="banner" id="home">
+      <div>
+        <span className="welcome">welcome to</span>
+        <h1>
+          <span>fit</span>mass
+        </h1>
+        <div>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate
+            aliquid nihil voluptas repellendus illo velit aliquam! Ea ipsam
+            dicta vero cum quo debitis voluptatem enim assumenda officiis
+            architecto! Nesciunt, cupiditate.
+          </p>
+        </div>
+        <button>Start</button>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
