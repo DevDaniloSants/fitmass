e3f7a6852947e259e40983c596045a9a26a8f616
Danilo Santos <dscofield@outlook.com.br>
2024-01-29 18:49:36 -0300

    :lipstick: feat: Estilização da área de programas

--- src/components/Programas.jsx
+++ src/components/Programas.jsx
@@ -1,39 +1,139 @@
 // styles
 import '../styles/components/programas.sass';
 
-//images
-import crossfit from '../assets/crossfit.png';
-import fitness from '../assets/fitness.png';
-import yoga from '../assets/yoga.png';
-import boxe from '../assets/boxe.png';
+import { motion } from 'framer-motion';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
+// hooks
+import { useState, useRef, useEffect, useLayoutEffect } from 'react';
+
+//images
+import crossfit from '../assets/crossfit.svg';
+import fitness from '../assets/fitness.svg';
+import yoga from '../assets/yoga.svg';
+import boxe from '../assets/boxe.svg';
+
+const programs = [
+  { id: 1, image: crossfit, name: 'Crossfit' },
+  { id: 2, image: fitness, name: 'Fitness' },
+  { id: 3, image: yoga, name: 'Yoga' },
+  { id: 4, image: boxe, name: 'Boxe' },
+];
 const Programas = () => {
+  const carousel = useRef();
+  const elements = useRef();
+  const timeLine = useRef();
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    const updateWidth = () => {
+      setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+    };
+
+    window.addEventListener('resize', updateWidth);
+
+    // Inital value
+    updateWidth();
+
+    // Remove listener
+    return () => {
+      window.removeEventListener('resize', updateWidth);
+    };
+  }, []);
+
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.context(() => {
+      timeLine.current = gsap
+        .timeline({
+          scrollTrigger: {
+            trigger: '.inner',
+            scrub: true,
+            start: 'top 600px',
+            end: 'bottom 850px',
+          },
+        })
+        .fromTo(
+          '#image1',
+          {
+            opacity: 0,
+            y: 160,
+          },
+          {
+            opacity: 1,
+            y: 0,
+          },
+        )
+        .fromTo(
+          '#image2',
+          {
+            opacity: 0,
+            y: 160,
+          },
+          {
+            opacity: 1,
+            y: 0,
+          },
+        )
+        .fromTo(
+          '#image3',
+          {
+            opacity: 0,
+            y: -160,
+          },
+          {
+            opacity: 1,
+            y: 0,
+          },
+        )
+        .fromTo(
+          '#image4',
+          {
+            opacity: 0,
+            y: -160,
+          },
+          {
+            opacity: 1,
+            y: 0,
+          },
+        );
+    }, elements);
+
+    return () => {
+      gsap.killTweensOf('.item');
+    };
+  }, []);
+
   return (
-    <div className="programs">
+    <div className="programs" ref={elements}>
       <div className="title">
         <h1>Outros Programas</h1>
       </div>
-      <div className="programsDetails">
-        <div>
-          <img src={crossfit} alt="Crossfit" />
-          <h1>CrossFit</h1>
-          <button>Detalhes</button>
-        </div>
-        <div>
-          <img src={fitness} alt="fitness" />
-          <h1>Fitness</h1>
-          <button>Detalhes</button>
-        </div>
-        <div>
-          <img src={yoga} alt="yoga" />
-          <h1>Yoga</h1>
-          <button>Detalhes</button>
-        </div>
-        <div>
-          <img src={boxe} alt="boxe" />
-          <h1>Boxe</h1>
-          <button>Detalhes</button>
-        </div>
+      <div className="carousel_container">
+        <motion.div
+          ref={carousel}
+          className="carousel"
+          whileTap={{ cursor: 'grabbing' }}
+        >
+          <motion.div
+            className="inner"
+            drag="x"
+            dragConstraints={{ right: 0, left: -width }}
+          >
+            {programs.map((program) => (
+              <motion.div
+                key={program.id}
+                className="item"
+                id={`image${program.id}`}
+              >
+                <img src={program.image} alt="teste" />
+                <span>{program.name}</span>
+                <button>Detalhes</button>
+              </motion.div>
+            ))}
+          </motion.div>
+        </motion.div>
       </div>
     </div>
   );
