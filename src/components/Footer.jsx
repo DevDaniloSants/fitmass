491b946b5276e8c4326d85606d314937e0d112e4
Danilo Santos <dscofield@outlook.com.br>
2024-01-30 01:17:40 -0300

    :art: feat(footer): Atualiza o footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,46 @@
 import '../styles/components/footer.sass';
 
 const Footer = () => {
-  return <footer>&copy; Todos os direitos reservados - 2024</footer>;
+  return (
+    <footer>
+      <div className="top_footer">
+        <span>FITMASS.</span>
+        <div className="links">
+          <ul>
+            <li>
+              <a href="#">Treinos</a>
+            </li>
+            <li>
+              <a href="#">Sobre nós</a>
+            </li>
+            <li>
+              <a href="#">Blog</a>
+            </li>
+            <li>
+              <a href="#">Horários de treino</a>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <a href="#">Política de Privacidade</a>
+            </li>
+            <li>
+              <a href="#">SAC</a>
+            </li>
+            <li>
+              <a href="#">Conheça nossas Academias</a>
+            </li>
+            <li>
+              <a href="#">Comece agora</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="end_footer">
+        <p>&copy; FITMASS Todos os Direitos Reservados. </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
